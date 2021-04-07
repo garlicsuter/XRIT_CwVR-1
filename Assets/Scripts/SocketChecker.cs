d09d4f7c07ed28de0558815871564e419788911c
Mark Suter <msuter@live.elida.k12.oh.us>
2021-04-07 09:59:27 -0400

    3.2 complete

---
+++ Assets/Scripts/SocketChecker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+
+public class SocketChecker : XRBaseInteractable
+{
+    private GameObject cap;
+
+    void Awake()
+    {
+        XRSocketInteractor socket = gameObject.GetComponent<XRSocketInteractor>();
+        //socket.onSelectEntered.AddListener(HatCheck);
+
+        cap = GameObject.FindGameObjectWithTag("BaseballHat");
+        Debug.Log("socket from Awake: " + socket);
+    }
+
+    public void HatCheck(XRBaseInteractable obj)
+    {
+        if (obj.gameObject.CompareTag("BaseballHat"))
+        {
+            Debug.Log("Destroy baseball hat!");
+            Destroy(cap);
+            
+        }
+    }
+}
