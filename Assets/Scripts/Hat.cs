7b6c23b947d0eb3305c237451ab51c09c0442910
Mark Suter <msuter@live.elida.k12.oh.us>
2021-04-27 11:15:40 -0400

    Changing to Direct Interactors

---
+++ Assets/Scripts/Hat.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+[System.Serializable]
+public class Hat : MonoBehaviour
+{
+    public GameObject Socket1;
+    public float myFloat = 0.09f;
+    public SocketChecker script;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        script = Socket1.GetComponent<SocketChecker>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // THIS WORKS!
+        //if (script.HatSnapped == true)
+        //{
+        //    transform.Rotate(10.0f, 10.0f, 10.0f);
+        //}
+        
+    }
+
+    //public void OnCollisionEnter(Collision other)
+    //{
+    //    if (other.gameObject.CompareTag("Drill") && script.HatSnapped == true)
+    //    {
+    //        Debug.Log("Drill hit HAAAATTTT");
+    //        //Debug.Log("SnappedObject ===" + SnappedObject);
+    //        //SnappedObject.transform.RotateAround(SnappedObject.transform.position, Vector3.up, 20 * Time.deltaTime);
+    //        gameObject.transform.Rotate(20.0f, 20.0f, 40.0f);
+    //    }
+    //}
+}
--- Assets/Scripts/SocketChecker.cs
+++ Assets/Scripts/SocketChecker.cs
@@ -7,23 +7,61 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class SocketChecker : XRBaseInteractable
 {
     private GameObject cap;
+    public GameObject capDummy;
+    public GameObject SnappedObject;
+    public bool HatSnapped = false;
+    public Vector3 preKillPos;
 
     void Awake()
     {
         XRSocketInteractor socket = gameObject.GetComponent<XRSocketInteractor>();
+
         //socket.onSelectEntered.AddListener(HatCheck);
+
+        //socket.selectEntered.AddListener();
 
         cap = GameObject.FindGameObjectWithTag("BaseballHat");
         Debug.Log("socket from Awake: " + socket);
     }
 
+  
     public void HatCheck(XRBaseInteractable obj)
     {
         if (obj.gameObject.CompareTag("BaseballHat"))
         {
-            Debug.Log("Destroy baseball hat!");
-            Destroy(cap);
+            Debug.Log("Hat Snapped!");
+            //Destroy(cap);
+            SnappedObject = obj.gameObject;
+            HatSnapped = true;
+            Debug.Log("Snapped Object Initial ~~~" + gameObject);
+
+        }
+    }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.CompareTag("Drill"))
+    //    {
+    //        Debug.Log("Drill hit Socket");
+    //        Debug.Log("SnappedObject ===" + SnappedObject);
+    //        //SnappedObject.transform.RotateAround(SnappedObject.transform.position, Vector3.up, 20 * Time.deltaTime);
+    //        SnappedObject.gameObject.transform.Rotate(20.0f, 20.0f, 40.0f);
+    //    }
+    //}
+
+    //
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Drill") && HatSnapped == true)
+        {
+            Debug.Log("Spinny?");
+            Debug.Log("Le Snapped Object =======" + SnappedObject);
+            //Debug.Log("SnappedObject ===" + SnappedObject);
             
+            preKillPos = SnappedObject.transform.position;
+            Destroy(SnappedObject);
+            Instantiate(capDummy, preKillPos, Quaternion.identity);
+           //transform.Rotate(120.0f, 20.0f, 40.0f);
         }
     }
 }
